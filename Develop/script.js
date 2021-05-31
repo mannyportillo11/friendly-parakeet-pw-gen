55bffae065bf544c01fe2ff8b7cb31adeea2f0bb
Man E <mannyportillo77@gmail.com>
2021-05-30 22:36:21 -0600

    add comments and update README

--- Develop/script.js
+++ Develop/script.js
@@ -5,6 +5,7 @@ var lowerCaseArr = "abcdefghijklmnopqrstuvwxyz".split("");
 var upperCaseArr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 var numberArray = "0123456789".split("");
 var symbolArray = "!@#$%^&*+~?=/;:`".split("");
+//create empty array
 var passwordArr = [];
 var promptPassword;
 
@@ -15,29 +16,29 @@ function genPassword() {
   
   //check for correct character input
   if (promptPassword > 7 && promptPassword < 129) {
-    var lowerCasePrompt = window.confirm(
+    var promptLower = window.confirm(
       "Would you like to include lowercase letters?"
     );
-    if (lowerCasePrompt === true) {
-      Array.prototype.push.apply(passwordArr, lowerCaseArr);
+    if (promptLower === true) {
+      Array.prototype.push.apply(passwordArr, lowerCaseArr); //includes item from lowercase array
     }
 
-    var upperCasePrompt = window.confirm("How about UPPERCASE LETTERS?");
-    if (upperCasePrompt === true) {
-      Array.prototype.push.apply(passwordArr, upperCaseArr);
+    var promptUpper = window.confirm("UPPERCASE LETTERS?");
+    if (promptUpper === true) {
+      Array.prototype.push.apply(passwordArr, upperCaseArr); //includes item from uppercase array
     }
 
-    var numberPrompt = window.confirm("Do you want numbers included?");
+    var numberPrompt = window.confirm("Would you like to include numbers?");
     if (numberPrompt === true) {
-      Array.prototype.push.apply(passwordArr, numberArray);
+      Array.prototype.push.apply(passwordArr, numberArray); //includes item from number array 
     }
 
-    var symbolPrompt = window.confirm("Maybe throw in some Symbol$?");
+    var symbolPrompt = window.confirm("Symbols?");
     if (symbolPrompt === true) {
-      Array.prototype.push.apply(passwordArr, symbolArray);
+      Array.prototype.push.apply(passwordArr, symbolArray);//includes item from symbol array 
     }
 
-    //check to make sure at least one type of characters is chosen
+    //checks for one type by ensuring length of array is at least 1 or more
     if (passwordArr.length === 0) {
       window.alert(
         "You must choose at least one type of character between lowercase, uppercase, numbers, and symbols. Please choose at least one."
@@ -46,7 +47,7 @@ function genPassword() {
       generatePass();
     }
   } else {
-    window.alert("Invalid password try again");
+    window.alert("Invalid password. Please, try again");
     genPassword();
   }
 }
